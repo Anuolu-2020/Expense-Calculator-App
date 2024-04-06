6708e97b16188f51063d97460adf055886bc6ef2
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-06 01:53:58 +0100

    feat: add logger middleware

--- client/main.go
+++ client/main.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/Anuolu-2020/Expense-Calculator-App/controllers"
+	"github.com/Anuolu-2020/Expense-Calculator-App/middleware"
 )
 
 //go:embed static
@@ -18,13 +19,13 @@ func main() {
 
 	router.Handle("/static/", http.FileServer(http.FS(static)))
 
-	router.HandleFunc("/", controllers.Home)
+	router.HandleFunc("GET /index", controllers.Home)
 
 	router.HandleFunc("GET /welcome", controllers.Welcome)
 
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: middleware.LoggerMiddleware(router),
 	}
 
 	log.Printf("Server listening on port %v", server.Addr)
