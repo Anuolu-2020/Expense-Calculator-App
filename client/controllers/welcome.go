307368a7c53a563681a206f7046455388a6bdd0d
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-06 14:43:15 +0100

    feat: welcome controller implemented

---
+++ client/controllers/welcome.go
@@ -0,0 +1,19 @@
+package controllers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func Welcome(w http.ResponseWriter, r *http.Request) {
+	message := "welcome to expense calculator welcome route"
+
+	responseMessage, err := json.Marshal(message)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(responseMessage))
+}
