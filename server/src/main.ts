b16103846fbc2295b768f55180094784ce53bd8d
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-13 20:03:13 +0100

    feat: enable versioning and add global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.enableVersioning({ type: VersioningType.URI, defaultVersion: '1' });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
