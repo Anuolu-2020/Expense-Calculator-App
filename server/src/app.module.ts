3b3f768b4036a22854aa88ac3e08f39005a6de27
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-13 17:51:48 +0100

    feat: add env variable validation before server startup

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,24 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 //import { APP_INTERCEPTOR } from '@nestjs/core';
 import { SummaryModule } from './summary/summary.module';
 import { ReportModule } from './report/report.module';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma/prisma.service';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [SummaryModule, ReportModule],
+  imports: [
+    SummaryModule,
+    ReportModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({ DATABASE_URL: Joi.string().required() }),
+      validationOptions: {
+        abortEarly: true,
+      },
+    }),
+  ],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
-export class AppModule {}
+export class AppModule { }
