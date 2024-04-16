b3f5df0bb9dce8aeef30953833e019412b4bc6ec
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-16 01:33:17 +0100

    feat: implement initialization of db

---
+++ client/db/db.go
@@ -0,0 +1,43 @@
+package db
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+
+	"github.com/Anuolu-2020/Expense-Calculator-App/models"
+)
+
+func Init() *gorm.DB {
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second,   // Slow SQL threshold
+			LogLevel:                  logger.Silent, // Log level
+			IgnoreRecordNotFoundError: true,          // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,          // Don't include params in the SQL log
+			Colorful:                  false,         // Disable color
+		},
+	)
+
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	db, err := gorm.Open(postgres.Open(DATABASE_URL), &gorm.Config{Logger: newLogger})
+	if err != nil {
+		fmt.Println("Error connecting to Database")
+		log.Fatal(err)
+	}
+
+	log.Println("DB migrating......")
+	db.AutoMigrate(&models.User{}, &models.Report{})
+	log.Println("DB migrated")
+
+	log.Println("Database connected.....")
+
+	return db
+}
