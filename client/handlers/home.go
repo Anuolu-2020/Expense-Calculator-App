38f4855db5faa93df9102977fb36a1b2f8d8b82b
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-16 01:34:28 +0100

    feat: implement dependency injection for handlers

--- client/handlers/home.go
+++ client/handlers/home.go
@@ -1,8 +1,17 @@
 package handlers
 
-import "net/http"
+import (
+	"net/http"
+
+	"gorm.io/gorm"
+)
 
 type Handler struct {
+	DB *gorm.DB
+}
+
+func New(DB *gorm.DB) Handler {
+	return Handler{DB}
 }
 
 func (h Handler) Home(w http.ResponseWriter, r *http.Request) {
