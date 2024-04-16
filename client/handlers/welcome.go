2ddc5679f989230a33a430be201165b34aa3d1a8
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-16 01:34:39 +0100

    chore: minor change

--- client/handlers/welcome.go
+++ client/handlers/welcome.go
@@ -10,10 +10,11 @@ func (h Handler) Welcome(w http.ResponseWriter, r *http.Request) {
 	message := "welcome to expense calculator welcome route"
 
 	responseMessage, err := json.Marshal(message)
-
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	w.Header().Set("Content-Type", "application/json")
 
 	w.Write([]byte(responseMessage))
 }
