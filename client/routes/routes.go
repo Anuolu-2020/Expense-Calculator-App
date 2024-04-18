53eed75e372ee10f33fdd026a30e1888f9851d3b
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-18 19:56:19 +0100

    feat: implement signin route

--- client/routes/routes.go
+++ client/routes/routes.go
@@ -3,7 +3,10 @@ package routes
 import (
 	"net/http"
 
+	"github.com/alexedwards/scs/v2"
+
 	"github.com/Anuolu-2020/Expense-Calculator-App/handlers"
+	"github.com/Anuolu-2020/Expense-Calculator-App/middleware"
 )
 
 type SetupRoute struct {
@@ -14,16 +17,16 @@ func (r *SetupRoute) New(newMux *http.ServeMux) {
 	r.mux = newMux
 }
 
-func (r SetupRoute) InitializeRoutes(handler handlers.Handler) {
+func (r SetupRoute) InitializeRoutes(handler handlers.Handler, SessionManager *scs.SessionManager) {
 	// main routes
 	r.mux.HandleFunc("GET /index", handler.Home)
-	r.mux.HandleFunc("GET /welcome", handler.Welcome)
-	r.mux.HandleFunc("POST /signup", handler.SignUp)
+	r.mux.HandleFunc("GET /welcome", middleware.CheckAuth(handler.Welcome, SessionManager))
 
 	apiRoutes := http.NewServeMux()
 
-	// Api Rooutes
+	// Api Routes
 	apiRoutes.HandleFunc("POST /signup", handler.SignUp)
+	apiRoutes.HandleFunc("POST /signin", handler.SignIn)
 
 	r.mux.Handle("/api/", http.StripPrefix("/api", apiRoutes))
 }
