63705999ed6a5d0d3de870c15817ee05932f46f4
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-18 19:52:50 +0100

    feat: implement session management

--- client/handlers/home.go
+++ client/handlers/home.go
@@ -3,15 +3,17 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/alexedwards/scs/v2"
 	"gorm.io/gorm"
 )
 
 type Handler struct {
-	DB *gorm.DB
+	DB      *gorm.DB
+	Session *scs.SessionManager
 }
 
-func New(DB *gorm.DB) Handler {
-	return Handler{DB}
+func New(DB *gorm.DB, Session *scs.SessionManager) Handler {
+	return Handler{DB, Session}
 }
 
 func (h Handler) Home(w http.ResponseWriter, r *http.Request) {
