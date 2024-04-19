58429ec9c7070ff38e1fefbf58d9232e512953ad
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-19 13:01:26 +0100

    feat: terminal now prints api routes

--- server/src/main.ts
+++ server/src/main.ts
@@ -24,5 +24,22 @@ async function bootstrap() {
 
   console.log(`listening on port ${3000}`);
   await app.listen(3000);
+
+  const server = app.getHttpServer();
+  const router = server._events.request._router;
+
+  const availableRoutes: [] = router.stack
+    .map((layer: any) => {
+      if (layer.route) {
+        return {
+          route: {
+            path: layer.route?.path,
+            method: layer.route?.stack[0].method,
+          },
+        };
+      }
+    })
+    .filter((item: any) => item !== undefined);
+  console.log(availableRoutes);
 }
 bootstrap();
