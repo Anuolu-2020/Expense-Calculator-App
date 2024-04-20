db6c9a078d590b08dc5bf5466cc253817e574eb7
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-20 18:57:45 +0100

    chore: renamed redirect route

--- client/middleware/checkAuth.go
+++ client/middleware/checkAuth.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"context"
+	"log"
 	"net/http"
 
 	"github.com/alexedwards/scs/v2"
@@ -9,15 +10,16 @@ import (
 
 func CheckAuth(next http.HandlerFunc, m *scs.SessionManager) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie := m.GetString(r.Context(), "username")
+		cookie := m.GetString(r.Context(), "userprofile")
 
-		if !m.Exists(r.Context(), "username") {
-			http.Redirect(w, r, "/sign-in", http.StatusFound)
+		if !m.Exists(r.Context(), "userprofile") {
+			log.Println("it doesn't exist")
+			http.Redirect(w, r, "/auth", http.StatusFound)
 			return
 		}
 
 		// Pass username to next handler e.g dashboard
-		ctx := context.WithValue(r.Context(), "profileName", cookie)
+		ctx := context.WithValue(r.Context(), "sessionData", cookie)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
 	})
