4a19137eb7b85e6e80a5a2df836175883c4134de
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-20 23:58:51 +0100

    fix: fixed session bug and implement cookie clearing

--- client/middleware/checkAuth.go
+++ client/middleware/checkAuth.go
@@ -4,16 +4,30 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/alexedwards/scs/v2"
 )
 
+func clearCookie(w http.ResponseWriter) {
+	cookie := &http.Cookie{
+		Name:    "session",
+		Value:   "",         // Clear the cookie value
+		MaxAge:  -1,         // Set MaxAge to negative value to expire immediately
+		Expires: time.Now(), // Set Expires to a time in the past
+		Path:    "/",        // Specify the path of the cookie you want to delete
+	}
+	http.SetCookie(w, cookie)
+}
+
 func CheckAuth(next http.HandlerFunc, m *scs.SessionManager) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie := m.GetString(r.Context(), "userprofile")
+		cookie := m.GetString(r.Context(), "userSession")
 
-		if !m.Exists(r.Context(), "userprofile") {
-			log.Println("it doesn't exist")
+		if exists := m.Exists(r.Context(), "userSession"); !exists {
+			log.Printf("Session exists?:%v", exists)
+			m.Destroy(r.Context()) //  Delete session
+			clearCookie(w)         // Clear cookie
 			http.Redirect(w, r, "/auth", http.StatusFound)
 			return
 		}
