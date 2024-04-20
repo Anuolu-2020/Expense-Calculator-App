bcfc0a99b24b7f61da54c4a8ed65b87549a16b7f
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-20 18:58:23 +0100

    feat: updated user model

--- client/models/models.go
+++ client/models/models.go
@@ -7,12 +7,14 @@ import (
 )
 
 type User struct {
-	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
-	Username  string    `gorm:"unique"`
-	Password  string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Reports   []Report `gorm:"foreignKey:UserID"`
+	ID         uuid.UUID `gorm:"type:uuid;default:gen_random_uuid()"`
+	Email      string    `gorm:"unique"`
+	Username   string
+	Password   string
+	ProfilePic string
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+	Reports    []Report `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;"`
 }
 
 type Report struct {
