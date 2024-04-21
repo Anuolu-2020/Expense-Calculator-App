a9d079c29ba084593d46040ee9cbb38fdea1a09f
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-21 15:03:55 +0100

    feat: implement fetching of results for dashboard

--- client/handlers/home.go
+++ client/handlers/home.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -13,6 +14,27 @@ import (
 type Handler struct {
 	DB      *gorm.DB
 	Session *scs.SessionManager
+}
+
+type Results struct {
+	ID        string `json:"id"`
+	Source    string `json:"source"`
+	Amount    int    `json:"amount"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+	UserId    string `json:"user_id"`
+	Type      string `json:"type"`
+}
+
+type Response struct {
+	Results []Results `json:"results"`
+}
+
+type TemplateData struct {
+	UserId   string
+	Username string
+	Photo    string
+	Results  []Results
 }
 
 func New(DB *gorm.DB, Session *scs.SessionManager) Handler {
@@ -36,11 +58,36 @@ func (h Handler) Dashboard(w http.ResponseWriter, r *http.Request) {
 	// Get user's username and photo
 	sessionData := ctx.Value("sessionData").(string)
 
-	data, err := pkg.DecodeSessionData(sessionData)
+	userData, err := pkg.DecodeSessionData(sessionData)
 	if err != nil {
 		log.Printf("Error occured while decoding session: %v", err)
 		http.Error(w, "An error occurred", http.StatusInternalServerError)
 		return
+	}
+
+	resp, err := http.Get(
+		"https://expense-calculator-api-j642.onrender.com/api/v1/report/" + userData.UserId,
+	)
+	if err != nil {
+		log.Printf("Error occured while fetching data: %v", err)
+		http.Error(w, "An error occurred", http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var response Response
+	if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+		log.Printf("Error occured while decoding data: %v", err)
+		http.Error(w, "An error occurred", http.StatusInternalServerError)
+		return
+	}
+
+	data := TemplateData{
+		UserId:   userData.UserId,
+		Username: userData.Username,
+		Photo:    userData.Photo,
+		Results:  response.Results,
 	}
 
 	pkg.SendTemplate(w, "dashboard.html", data)
