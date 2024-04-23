f115defc19a5ebf4827e88bde643538582eaa1d3
Anuolu-2020 <Anuolu1645@gmail.com>
2024-04-23 23:45:57 +0100

    feat: implement create user report handler

--- client/handlers/reports.go
+++ client/handlers/reports.go
@@ -1,12 +1,21 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/Anuolu-2020/Expense-Calculator-App/pkg"
 )
+
+type Report struct {
+	Type   string      `json:"type"`
+	Source string      `json:"source"`
+	Amount json.Number `json:"amount"`
+}
 
 func (h Handler) GetUserReports(w http.ResponseWriter, r *http.Request) {
 	// user id
@@ -31,4 +40,57 @@ func (h Handler) GetUserReports(w http.ResponseWriter, r *http.Request) {
 
 	// Send template
 	pkg.SendTemplate(w, "reports.html", response)
+}
+
+// Send a request to the backend api to create a report
+func (h Handler) CreateUserReport(w http.ResponseWriter, r *http.Request) {
+	// Get user id
+	userId := r.PathValue("userId")
+
+	var userRequest Report
+	err := json.NewDecoder(r.Body).Decode(&userRequest)
+	if err != nil {
+		log.Printf("Error decoding json: %v", err)
+		pkg.ServeErrorPage(w, r)
+		return
+	}
+
+	// Check report type
+	if userRequest.Type != "income" && userRequest.Type != "expense" {
+		fmt.Println(w, "Type must be income or expense")
+		return
+	}
+
+	// Check if amount is negative
+	// Assuming userRequest.Amount is of type json.Number
+	if amountInt, err := strconv.Atoi(userRequest.Amount.String()); err == nil && amountInt <= 0 {
+		fmt.Println(w, "Amount must not be negative")
+	}
+
+	// Convert request body to json
+	reqBody, err := json.Marshal(&userRequest)
+	if err != nil {
+		log.Printf("Error encoding json: %v", err)
+		pkg.ServeErrorPage(w, r)
+		return
+	}
+
+	resp, err := http.Post(
+		"https://expense-calculator-api-j642.onrender.com/api/v1/report/"+userRequest.Type+"/"+userId,
+		"application/json",
+		bytes.NewBuffer(reqBody),
+	)
+	if err != nil {
+		log.Printf("Error sending api request:%v", err)
+		pkg.ServeErrorPage(w, r)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	var userData Results
+
+	json.NewDecoder(resp.Body).Decode(&userData)
+
+	pkg.SendTemplate(w, "newReport.html", userData)
 }
