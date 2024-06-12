def0e448c80a11c1d20f196115b4d2932ff256cf
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-12 19:58:24 +0100

    feat: implement pie chart generator handlers

--- client/handlers/pieChart.go
+++ client/handlers/pieChart.go
@@ -11,35 +11,67 @@ import (
 )
 
 func GetPieChartItems(reports Response) []opts.PieData {
-	items := make([]opts.PieData, 0)
+	summedReports := make([]Results, 0)
 
 	for _, report := range reports.Results {
+		found := false
+
+		for idx, summedReport := range summedReports {
+			if summedReport.Source == report.Source {
+				summedReports[idx].Amount += report.Amount
+				found = true
+				break
+			}
+		}
+
+		if !found {
+			summedReports = append(summedReports, report)
+		}
+	}
+
+	items := make([]opts.PieData, 0)
+
+	for _, report := range summedReports {
 		items = append(items, opts.PieData{Name: report.Source, Value: report.Amount})
 	}
 
 	return items
 }
 
-func GeneratePieChart(reports Response) *charts.Pie {
+// Generate Pie chart
+func GeneratePieChart(reports Response, w http.ResponseWriter) {
 	pie := charts.NewPie()
 
-	pie.SetGlobalOptions(
-		charts.WithTitleOpts(opts.Title{Title: "Your reports so far"}),
+	pie.PageTitle = "Expense Tracker - Report Graph"
+
+	pie.AddSeries("pie", GetPieChartItems(reports)).SetSeriesOptions(
+		charts.WithLabelOpts(opts.Label{Show: true, Formatter: "{b}: {c}"}),
 	)
 
-	pie.AddSeries("pie", GetPieChartItems(reports))
+	// Use the custom Renderer
+	//	pie.Renderer = graph.NewSnippetRenderer(pie, pie.Validate)
 
-	return pie
+	pie.Render(w)
 }
 
-func (h Handler) PieChart(w http.ResponseWriter, r *http.Request) {
-	reports, err := GetReports(r)
+func GenerateReportPieChart(userId string, w http.ResponseWriter) {
+	reports, err := GetReports(userId)
 	if err != nil {
 		log.Printf("Error occurred while retrieving reports: %v", err)
-		pkg.ServeErrorPage(w, r)
+		pkg.SendErrorResponse(w, "An Error Occurred", http.StatusInternalServerError)
+		return
 	}
 
-	bar := GeneratePieChart(*reports)
+	GeneratePieChart(*reports, w)
+}
 
-	bar.Render(w)
+func GenerateReportTypePieChart(reportType string, userId string, w http.ResponseWriter) {
+	reports, err := GetReportsType(reportType, userId)
+	if err != nil {
+		log.Printf("Error occurred while retrieving reports: %v", err)
+		pkg.SendErrorResponse(w, "An Error occurred", http.StatusInternalServerError)
+		return
+	}
+
+	GeneratePieChart(*reports, w)
 }
