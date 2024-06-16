482ca7d1d53dad4505d3a114ac37e504b4076b0a
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-16 14:39:10 +0100

    feat: rate limiter added

--- client/server.go
+++ client/server.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"context"
 	"embed"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/gomodule/redigo/redis"
 	"github.com/joho/godotenv"
+	"github.com/sethvargo/go-limiter"
+	"github.com/sethvargo/go-limiter/httplimit"
+	"github.com/sethvargo/go-redisstore"
 
 	"github.com/Anuolu-2020/Expense-Calculator-App/db"
 	"github.com/Anuolu-2020/Expense-Calculator-App/handlers"
@@ -16,6 +26,29 @@ import (
 
 //go:embed static
 var static embed.FS
+
+// Connect to redis for rate limiter store
+func ConnectRedis() limiter.Store {
+	// ctx := context.Background()
+
+	addr := os.Getenv("REDIS_ADDR")
+
+	store, err := redisstore.New(&redisstore.Config{
+		Tokens:   40,
+		Interval: time.Minute,
+		Dial: func() (redis.Conn, error) {
+			return redis.DialURL(addr)
+		},
+	})
+	if err != nil {
+		log.Printf("[ERROR]: Error connecting to redis: %v", err)
+		log.Fatal(err)
+	}
+	// defer store.Close(ctx)
+	log.Print("Connected to Redis Successfully")
+
+	return store
+}
 
 func main() {
 	//go:generate npm run build
@@ -40,6 +73,14 @@ func main() {
 	// Initailize Session
 	sessionManager := middleware.InitSession()
 
+	// Connect to redis
+	store := ConnectRedis()
+
+	rateLimiterMiddleware, err := httplimit.NewMiddleware(store, httplimit.IPKeyFunc())
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	// Initialize Google Config
 	pkg.InitGoogle()
 
@@ -56,12 +97,39 @@ func main() {
 		sessionManager.LoadAndSave,
 	)
 
+	// Rate limit all routes
+	wrappedRouter := stacks(rateLimiterMiddleware.Handle(router))
+
 	server := &http.Server{
 		Addr:    ":8080",
-		Handler: stacks(router),
+		Handler: wrappedRouter,
 	}
+
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Printf("Could not listen on :8080: %v\n", err)
+			log.Fatal(err)
+		}
+	}()
 
 	log.Printf("Server listening on port %v", server.Addr)
 
-	server.ListenAndServe()
+	// Channel to listen for interrupt or terminate signals from the OS
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	// Block until we receive our signal
+	<-stopChan
+	log.Println("Shutting down server...")
+
+	// Create a context with a timeout to allow the server to shutdown gracefully
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Shutdown the server
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v\n", err)
+	} else {
+		log.Println("Server stopped gracefully")
+	}
 }
