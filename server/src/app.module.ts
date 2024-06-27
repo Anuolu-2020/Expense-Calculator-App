17d2b9aa58e296e851e532154e7b381962dfaf24
Anuolu-2020 <Anuolu1645@gmail.com>
2024-06-27 23:09:17 +0100

    feat: added rate limiting

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -6,9 +6,30 @@ import { PrismaService } from './prisma/prisma.service';
 import * as Joi from 'joi';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import {
+  ThrottlerGuard,
+  ThrottlerModule,
+  hours,
+  seconds,
+} from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          limit: Number(process.env.RATE_LIMIT_PER_MINUTE),
+          ttl: seconds(Number(process.env.THROTTLE_TTL1)),
+        },
+        {
+          limit: Number(process.env.RATE_LIMIT_PER_HOUR),
+          ttl: hours(Number(process.env.THROTTLE_TTL2)),
+        },
+      ],
+      storage: new ThrottlerStorageRedisService(process.env.REDIS_URL),
+    }),
     SummaryModule,
     ReportModule,
     ConfigModule.forRoot({
@@ -16,6 +37,11 @@ import { AppController } from './app.controller';
       validationSchema: Joi.object({
         DATABASE_URL: Joi.string().required(),
         PORT: Joi.number().required(),
+        REDIS_URL: Joi.string().required(),
+        RATE_LIMIT_PER_MINUTE: Joi.string().required(),
+        THROTTLE_TTL1: Joi.string().required(),
+        RATE_LIMIT_PER_HOUR: Joi.string().required(),
+        THROTTLE_TTL2: Joi.string().required(),
       }),
       validationOptions: {
         abortEarly: true,
@@ -23,6 +49,13 @@ import { AppController } from './app.controller';
     }),
   ],
   controllers: [AppController],
-  providers: [PrismaService, AppService],
+  providers: [
+    PrismaService,
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule { }
